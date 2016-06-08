c637926d6c805dd9cf126ee8317130c7178b57c1
BinaryEvolved <me@binaryevolved.com>
2016-06-08 01:22:59 -0400

    Basic refactor and alignment

--- betwixt.php
+++ betwixt.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Betwixt, A PHP Based system to help regulate bots, and malicious users.
  * https://github.com/BinaryEvolved/Betwixt
@@ -10,15 +11,14 @@
  *  Once included it's code will automatically run and perform it's protection checks.
  * @version 1.0
  */
-
-class Betwixt{
-
-
+class Betwixt
+{
 
 
-private $config = array();
+    private $config = array();
 
-    public function __construct(){
+    public function __construct()
+    {
         /*
          * Begin Configuration
         */
@@ -97,114 +97,122 @@ private $config = array();
     }
 
 
+    /**
+     * Checks for three cookies and ensures they are valid, returns true or false depending on validity of tokens
+     * @return bool
+     */
+    public function CheckToken()
+    {
+        $tokenID = $_COOKIE[$this->CraftCookieName('ID')];
+        $tokenData = $_COOKIE[$this->CraftCookieName('Token')];
+        $tokenTime = $_COOKIE[$this->CraftCookieName('Timestamp')];
+        $serverToken = $tokenID . '|' . $tokenTime . '|' . $this->config['ipSource'];
+        if ($this->config['userAgentLock']) $serverToken .= '|' . $_SERVER['HTTP_USER_AGENT'];
+        $serverToken = $this->HashKey($serverToken);
+        if (!$this->config['enabled']) return true;
+        if (hash_equals($serverToken, $tokenData)){
+            if ($tokenTime < time()) return false;
+            return true;
+        }
+        else{
+            return false;
+        }//If all else fails, fail script
 
+    }
 
-/**
- * Checks for three cookies and ensures they are valid, returns true or false depending on validity of tokens
- * @return bool
- */
-public function CheckToken(){
-    $tokenID = $_COOKIE[$this->CraftCookieName('ID')];
-    $tokenData = $_COOKIE[$this->CraftCookieName('Token')];
-    $tokenTime = $_COOKIE[$this->CraftCookieName('Timestamp')];
-    $serverToken = $tokenID.'|'.$tokenTime.'|'.$this->config['ipSource'];
-    if ($this->config['userAgentLock']) $serverToken .= '|'.$_SERVER['HTTP_USER_AGENT'];
-    $serverToken = $this->HashKey($serverToken);
-    if (!$this->config['enabled']) return true;
-    if (hash_equals($serverToken, $tokenData)){
-        if ($tokenTime < time()) return false;
+    /**
+     * Formats a cookie name depending on the global configuration
+     * @param $cookieID
+     * @return string
+     */
+    public function CraftCookieName($cookieID)
+    {
+        return $this->config['cookiePrefix'] . '_' . $cookieID;
+    }
+
+    /**
+     * Hashes and keys several values togethers and sets them into three different cookies. Returns false on failure
+     * @return bool
+     */
+    public function Imprint()
+    {
+        $tokenID = bin2hex(openssl_random_pseudo_bytes(5));//Generates a token ID
+        $expiryTime = time() + $this->config['cookieExpiry'];
+        $tokenValue = $tokenID . '|' . $expiryTime . '|' . $this->config['ipSource'];
+        if ($this->config['userAgentLock']) $tokenValue .= '|' . $_SERVER['HTTP_USER_AGENT'];
+        $cookieToken = $this->HashKey($tokenValue);
+        if (!$cookieToken) return false;
+        $domainPath = $this->config['cookiePath'] == '' ? false : $this->config['cookiePath'];
+        setcookie($this->CraftCookieName('ID'), $tokenID, $expiryTime, $this->config['cookiePath'], $domainPath,
+                  $this->config['cookieSecure'], true);
+        setcookie($this->CraftCookieName('Token'), $cookieToken, $expiryTime, $this->config['cookiePath'], $domainPath,
+                  $this->config['cookieSecure'], true);
+        setcookie($this->CraftCookieName('Timestamp'), $expiryTime, $expiryTime, $this->config['cookiePath'], $domainPath,
+                  $this->config['cookieSecure'], true);
         return true;
-    }else{
-        return false;
-    }//If all else fails, fail script
+    }
 
-}
+    /**
+     * Hashs and keys input into a hash, fails if unable to retrieve key
+     * @param $input
+     * @return bool|string
+     */
+    public function HashKey($input)
+    {
+        $key = $this->GetKey();
+        if (!$key){
+            trigger_error('Betwixt: Unable to retrieve a safe key');
+            return false;
+        }
+        $output = hash_hmac('sha512', $input, $key);
+        return $output;
+    }
 
-/**
- * Formats a cookie name depending on the global configuration
- * @param $cookieID
- * @return string
- */
-    public function CraftCookieName($cookieID){
-    return $this->config['cookiePrefix'].'_'.$cookieID;
-}
-
-/**
- * Hashes and keys several values togethers and sets them into three different cookies. Returns false on failure
- * @return bool
- */
-    public function Imprint(){
-    $tokenID = bin2hex(openssl_random_pseudo_bytes(5));//Generates a token ID
-    $expiryTime = time() + $this->config['cookieExpiry'];
-    $tokenValue = $tokenID.'|'.$expiryTime.'|'.$this->config['ipSource'];
-    if ($this->config['userAgentLock']) $tokenValue .= '|'.$_SERVER['HTTP_USER_AGENT'];
-    $cookieToken = $this->HashKey($tokenValue);
-    if (!$cookieToken)return false;
-    $domainPath = $this->config['cookiePath'] == '' ? false : $this->config['cookiePath'];
-    setcookie ($this->CraftCookieName('ID'), $tokenID, $expiryTime, $this->config['cookiePath'], $domainPath,
-               $this->config['cookieSecure'], true);
-    setcookie ($this->CraftCookieName('Token'), $cookieToken, $expiryTime, $this->config['cookiePath'], $domainPath,
-               $this->config['cookieSecure'], true);
-    setcookie ($this->CraftCookieName('Timestamp'), $expiryTime, $expiryTime, $this->config['cookiePath'], $domainPath,
-               $this->config['cookieSecure'], true);
-    return true;
-}
-
-/** 
- * Hashs and keys input into a hash, fails if unable to retrieve key
- * @param $input
- * @return bool|string
- */
-    public function HashKey($input){
-    $key = $this->GetKey();
-    if (!$key){
-        trigger_error ('Betwixt: Unable to retrieve a safe key');
+    /**
+     * Returns key, if key global variable is empty it creates and reads a 2048 bit key itself.
+     * @return bool|string
+     */
+    public function GetKey()
+    {
+        if ($this->config['key']) return $this->config['key'];
+        $file = dirname(__FILE__) . '/' . '/betwixt-key.php';
+        $key = '';//Pleases IDEs
+        if (file_exists($file)){
+            include $file;
+            return hex2bin($key);
+        }
+        if (is_writable(dirname(__FILE__))){
+            $key = bin2hex(openssl_random_pseudo_bytes(256));
+            $fh = fopen($file, 'w');
+            fwrite($fh, '<?php $key = "' . $key . '";' . PHP_EOL);
+            fclose($fh);
+            return $key;
+        }
         return false;
     }
-    $output = hash_hmac('sha512', $input, $key);
-    return $output;
-}
 
-/**
- * Returns key, if key global variable is empty it creates and reads a 2048 bit key itself.
- * @return bool|string
- */
-    public function GetKey() {
-    if ($this->config['key']) return $this->config['key'];
-    $file = dirname(__FILE__).'/'.'/betwixt-key.php';
-    $key = '';//Pleases IDEs
-    if (file_exists($file)) {
-        include $file;
-        return hex2bin($key);
+    /**
+     * Checks if the client has the proper cookies set, does not validate the cookies.
+     * @return bool
+     */
+    public function IsActive()
+    {
+        if (isset ($_COOKIE[$this->CraftCookieName("ID")]) && isset ($_COOKIE[$this->CraftCookieName("Token")])
+            && isset ($_COOKIE[$this->CraftCookieName("Timestamp")])
+        ){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
-    if (is_writable(dirname(__FILE__))) {
-        $key = bin2hex(openssl_random_pseudo_bytes(256));
-        $fh = fopen($file, 'w');
-        fwrite($fh, '<?php $key = "'.$key.'";' . PHP_EOL);
-        fclose($fh);
-        return $key;
-    }
-    return false;
-}
-
-/**
- * Checks if the client has the proper cookies set, does not validate the cookies.
- * @return bool
- */
-    public function IsActive(){
-    if (isset ($_COOKIE[$this->CraftCookieName("ID")]) && isset ($_COOKIE[$this->CraftCookieName("Token")])
-        && isset ($_COOKIE[$this->CraftCookieName("Timestamp")])){
-        return true;
-    }else{
-        return false;
-    }
-}
 
     /**
      * Returns the config setting for the display page
      * @return string
      */
-    public function GetDisplayPage(){
+    public function GetDisplayPage()
+    {
         return $this->config['displayPage'];
     }
 
@@ -213,40 +221,44 @@ public function CheckToken(){
      * Returns the enabled status as set in the config
      * @return string
      */
-    public function IsEnabled(){
+    public function IsEnabled()
+    {
         return $this->config['enabled'];
     }
 }
+
 /*
  * End Functions
  * Begin Initialization
  */
 $betwixt = new Betwixt();
 
-if ($betwixt->IsEnabled()) {//Check if Betwixt is enabled, and allowed to run
+if ($betwixt->IsEnabled()){//Check if Betwixt is enabled, and allowed to run
     if ($betwixt->IsActive()){//Checks if a Betwixt token has been declared for the user
         if (!$betwixt->CheckToken()){
-            setcookie($betwixt->CraftCookieName('ID'), "", time()-3600);
-            setcookie($betwixt->CraftCookieName('Token'), "", time()-3600);
-            setcookie($betwixt->CraftCookieName('Timestamp'), "", time()-3600);
+            setcookie($betwixt->CraftCookieName('ID'), "", time() - 3600);
+            setcookie($betwixt->CraftCookieName('Token'), "", time() - 3600);
+            setcookie($betwixt->CraftCookieName('Timestamp'), "", time() - 3600);
             echo "Error- Your Betwixt Validation Token is invalid. You have either forged a token, or have been idle too
             long. Try to refresh the page. If this error continues to occur, contact the webmaster. Please ensure you have enabled cookies.";
             die();//Kill the rest of the page to prevent sensitive material from loading
 
         }
-    }else{
+    }
+    else{
         //Creates a set of tokens for validation
         if (!$betwixt->Imprint()){
             echo "<h1>An error occurred, please ensure you have cookies enabled</h1>";
             echo "<br><p>-Betwixt</p>";
-        }else{
+        }
+        else{
 
-        //Render page
-        $include = dirname(__FILE__).'/'.$betwixt->GetDisplayPage();
-        include($include);
-        //Token is created, Refresh page to re-run the check
-        echo '<meta http-equiv="refresh" content="3">';
-        die();//Stop the rest of the page from rendering.
+            //Render page
+            $include = dirname(__FILE__) . '/' . $betwixt->GetDisplayPage();
+            include($include);
+            //Token is created, Refresh page to re-run the check
+            echo '<meta http-equiv="refresh" content="3">';
+            die();//Stop the rest of the page from rendering.
         }
     }
 }
